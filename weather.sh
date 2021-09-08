efe2be123d02200d5710c25c33b07c406528aade
Lucas <2366369366@qq.com>
2021-09-08 20:32:26 +0800

    Update weather.sh
    CHANGE CITY

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 set -eux
 
-CITY=Shanghai
+CITY=Chengdu
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
